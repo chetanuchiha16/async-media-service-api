172e97bbee19e7005d8e588215be2c0291377a79
chetanuchiha16 <chetan16ck@gmail.com>
2026-02-15 23:03:35 +0530

    enabled cors

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import api_router
 from app.db.db import create_db_and_tables
@@ -15,6 +16,17 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,  # Allow cookies to be sent cross-origin
+    allow_methods=["*"],  # Allow all methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(api_router, prefix="/api")
 app.include_router(
